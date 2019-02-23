fb35ed700dbc3f4712a0463b83245bd54528eb5f
Josh Price <joshua.price88@gmail.com>
2019-02-23 13:19:24 -0600

    fixed sytax errors

--- week-1/price-assignment-1.5.js
+++ week-1/price-assignment-1.5.js
@@ -1,5 +1,23 @@
+var header = require('../price-header.js');
+var header = header.display("Josh", "Price", "Exercise 1.5");
+console.log(header + '\n');
+
+/*
+============================================
+; Title:  price-exercise-1.5.js
+; Author: Josh Price
+; Date:   23 February 2019
+; Modified By: Josh Price
+; Description: Hello World! 
+;===========================================
+*/
+
+//start program
+
+//Requiring HTTP
 var http = require('http');
 
+//Function to request server body
 function bodyRequest(req, res) {
     var body = "Cause it's so catchy, catchy It's such a catchy song Gonna make you happy, happy I'm tryna code and sing along";
     var contentLength = body.length;
@@ -12,8 +30,12 @@ function bodyRequest(req, res) {
     res.end(body);
 }
 
+//Creating and listening to server
 var app = http.createServer(bodyRequest);
 
 app.listen(8080);
 
+//Logging to make sure server is running
 console.log('Server start on port 8080');
+
+//End Program

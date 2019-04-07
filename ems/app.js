5492a63e23c3d26f2b64f8afc6d32c25dc32074e
Josh Price <joshua.price88@gmail.com>
2019-04-07 11:47:03 -0500

    Pushing Milestone 3

--- ems/app.js
+++ ems/app.js
@@ -1,23 +1,29 @@
-var header = require('../../price-header.js');
-var header = header.display("Josh", "Price", "Assignment 6.4");
-console.log(header + '\n');
-
-/*
-============================================
-; Title:  price-assignment-6.4.js
-; Author: Josh Price
-; Date:   30 March 2019
-; Modified By: Josh Price
-; Description: Milestone 2 UI
-;===========================================
-*/
-
 //Start Program
 var express = require('express');
 var http = require ('http');
 var path = require('path');
 var logger = require('morgan');
+var mongoose = require("mongoose");
+var employee = require('./models/employee');
 
+//Connecting to MongoDB Database
+var mongoDB = "mongodb+srv://joshuaprice1:test123@ems-bm9ej.mongodb.net/test?retryWrites=true"
+
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+db.once("open", function() {
+    console.log("Application connected to Atlas MongoDB instance")
+});
+
+//Application
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
@@ -27,6 +33,11 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(logger("short"));
 
+var employee = new employee({
+    firstName: "Jon",
+    lastName: "Smith"
+});
+
 app.get("/", function (req, res) {
     res.render("index", {
         title:"Home",
@@ -34,6 +45,7 @@ app.get("/", function (req, res) {
     });
 });
 
+//Creating Server
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port 8080!");
 });
